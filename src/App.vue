f5779d904c9b1b48a3326d8c5a0dc4541848e52f
Matus <mucka@netsearch.cz>
2020-03-09 09:21:50 +0100

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <cytoscape
+    ref="cy"
+    :config="config"
+    v-on:mousedown="addNode"
+    v-on:cxttapstart="updateNode"
+  >
+    <cy-element
+      class="cytospace"
+      v-for="def in elements"
+      :key="`${def.data.id}`"
+      :definition="def"
+      v-on:mousedown="deleteNode($event, def.data.id)"
+    />
+  </cytoscape>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default Vue.extend({
   name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      config: {
+        style: [
+          {
+            selector: "node",
+            style: {
+              "background-color": "#666",
+              label: "data(id)"
+            }
+          },
+          {
+            selector: "edge",
+            style: {
+              width: 3,
+              "line-color": "#ccc",
+              "target-arrow-color": "#ccc",
+              "target-arrow-shape": "triangle"
+            }
+          }
+        ],
+        layout: {
+          name: "grid",
+          rows: 1
+        }
+      },
+      elements: [
+        {
+          // node a
+          data: { id: "a" }
+        },
+        {
+          // node b
+          data: { id: "b" }
+        },
+        {
+          // edge ab
+          data: { id: "ab", source: "a", target: "b" }
+        }
+      ]
+    };
   }
 });
 </script>
@@ -24,6 +72,17 @@ export default Vue.extend({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+html {
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+* {
+  height: 100%;
 }
 </style>
